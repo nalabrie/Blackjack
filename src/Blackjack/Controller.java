62d97d59b15cc40337b765b07d8f7e5fd008304a
nalabrie <ku9ca7cmlk@gmail.com>
2018-03-11 10:26:58 -0400

    betPressed() method finished.

--- src/Blackjack/Controller.java
+++ src/Blackjack/Controller.java
@@ -1,5 +1,7 @@
 package Blackjack;
 
+import Blackjack.Exceptions.BetTooBigException;
+import Blackjack.Exceptions.InvalidBetException;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
@@ -38,11 +40,42 @@ public class Controller {
     private Wallet dealerWallet = new Wallet();
     private Wallet playerWallet = new Wallet();
 
+    // set the starting state of the game
+
+    public void initialize() {
+
+        // set all labels to their starting values
+
+        dealerMoneyLabel.setText(String.valueOf(dealerWallet.getMoney()));
+        playerMoneyLabel.setText(String.valueOf(playerWallet.getMoney()));
+        currentBetLabel.setText(String.valueOf(playerWallet.getBet()));
+    }
+
     // event handlers for GUI elements
 
     @FXML
     private void betPressed(ActionEvent event) {
+        try {
+            // set the player bet to the value in the betTextField
+            playerWallet.setBet(Integer.parseInt(betTextField.getText()));
 
+            // update currentBetLabel
+            currentBetLabel.setText(String.valueOf(playerWallet.getBet()));
+
+            // disable button and text field until next round
+            betButton.setDisable(true);
+            betTextField.setDisable(true);
+        } catch (NumberFormatException | InvalidBetException e) {
+            // invalid input (bet is not an integer or <= 0) so focus on the input error and try again
+            betTextField.setText("Invalid bet");
+            betTextField.selectAll();
+            betTextField.requestFocus();
+        } catch (BetTooBigException e) {
+            // invalid input (bet is > player money) so focus on the input error and try again
+            betTextField.setText("Bet too big");
+            betTextField.selectAll();
+            betTextField.requestFocus();
+        }
     }
 
     @FXML
